7a52635bbc7c0c056e46eeac2a79df320fcaf892
Khoa Pham <phamdangkhoa0403@gmail.com>
2017-11-15 11:31:41 +0700

    update project v0.2 - menubar - filter

---
+++ WebPPCRental/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPPCRental.Models;
+
+namespace WebPPC.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoPPCRentalEntities1 db = new DemoPPCRentalEntities1();
+        public ActionResult Index()
+        {
+            List<PROPERTY> mi = new List<PROPERTY>();
+            mi = db.PROPERTies.ToList();
+            return View(mi);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Detail(int id)
+        {
+            PROPERTY team2 = db.PROPERTies.Find(id);
+            return View(team2);
+        }
+        public ActionResult Filter(string text)
+        {
+            var team2 = db.PROPERTies.ToList().Where(x => x.DISTRICT.ToString().Contains(text));
+            return View(team2);
+        }
+    }
+}
---
+++ WebPPCRental/Controllers/PropertyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPPCRental.Models;
+
+namespace WebPPCRental.Controllers
+{
+    public class PropertyController : Controller
+    {
+        DemoPPCRentalEntities1 model = new DemoPPCRentalEntities1();
+        //
+        // GET: /Property/
+        public ActionResult Detail(int id)
+        {
+            PROPERTY product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            return View(product);
+        }
+	}
+}
